633569c636832033bd6c20fad5a010ca909a6ff7
Nicholas Eduardo <nicholasess@gmail.com>
2018-08-30 18:39:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# docz-plugin-netlify
+
+This package makes it possible to use netlify with docz
+
+```js
+import doczPluginNetlify from "docz-plugin-netlify";
+
+export default {
+  plugins: [doczPluginNetlify()]
+};
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let { createPlugin } = require("docz-core");
+
+module.exports = () =>
+  createPlugin({
+    onPostBuild: () => {
+      fs.writeFile(
+        "/.docz/dist/_redirects",
+        "/*    /index.html   200",
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
+
+          console.log("The file was saved!");
+        }
+      );
+    }
+  });
