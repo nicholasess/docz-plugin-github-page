e04b7bc889573826a15a8d72c2588909b63db438
Nicholas Eduardo <nicholasess@gmail.com>
2018-08-30 19:14:05 -0300

    fix write file

--- index.js
+++ index.js
@@ -4,16 +4,8 @@ let { createPlugin } = require("docz-core");
 module.exports = () =>
   createPlugin({
     onPostBuild: () => {
-      fs.writeFile(
-        "./.docz/dist/_redirects",
-        "/*    /index.html   200",
-        function(err) {
-          if (err) {
-            return console.log(err);
-          }
-
-          console.log("The file was saved!");
-        }
-      );
+      fs.writeFileSync("./.docz/dist/_redirects", "/*    /index.html   200", {
+        encoding: "utf8"
+      });
     }
   });
